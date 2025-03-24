b6a1388b7a8966beac796940476a54baa1057f69
snowspar7891 <someshkumar7891@gmail.com>
2025-03-25 03:30:41 +0530

    homepage1

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,17 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React, { ReactNode } from 'react'
 
 const RootLayout = ({children}:{children: ReactNode}) => {
   return (
-    <div>
+    <div className='root-layout'>
+        <nav>
+          <Link href="/" className='flex items-center gap-2'>
+            <Image src="/logo.svg" alt="logo" width={40} height={32} />
+            <h2 className='text-primary-200'>PrepWise</h2>
+          </Link>
+        </nav>
+
         {children}
     </div>
   )
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,26 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Page = () => {
     return (
-        <div>Page Hi their 👋
-           <br/> HomePage
-        </div>
+       <>
+       <section className='card-cta'>
+           <div className='flex flex-col  gap-6 max-w-lg'>
+             <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
+             <p className='text-lg'>
+                Practice on real interview questions & get instant feedback .
+             </p>
+             <Button asChild className='btn-primary max-sm:w-full'>
+              <Link href='/interview'>
+               Start an Interview
+              </Link>
+             </Button>
+           </div>
+           <Image src='/robot.png' alt='bot-ken' width={400} height={400} className='max-sm:hidden'/>
+       </section>
+       </>
     )
 }
 export default Page
